ad8c45e72f9cd56d7456e60fff1e7dd067155f6e
Sujay <sujayvadavadagi@gmail.com>
2020-10-25 19:35:45 +0530

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAFJ78foqaGFhNdsRVxfTWVE_qWbWHbco8",
+  authDomain: "sujay-slack.firebaseapp.com",
+  databaseURL: "https://sujay-slack.firebaseio.com",
+  projectId: "sujay-slack",
+  storageBucket: "sujay-slack.appspot.com",
+  messagingSenderId: "303699278750",
+  appId: "1:303699278750:web:eabb7c15b0d24b80c6b48f",
+  measurementId: "G-4408RF5P1X",
+};
